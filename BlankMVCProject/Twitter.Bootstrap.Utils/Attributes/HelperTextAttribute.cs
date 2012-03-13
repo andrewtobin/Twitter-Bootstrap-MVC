242ee8b179c186f33d9f7101102cb807f203a2d9
Gary K <garyk46@gmail.com>
2012-03-13 12:19:16 +0000

    Added project containing some simple bootstrap extension methods.

---
+++ BlankMVCProject/Twitter.Bootstrap.Utils/Attributes/HelperTextAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace Twitter.Bootstrap.Utils.Attributes
+{
+    // Form help text
+    public class HelperTextAttribute : Attribute, IMetadataAware
+    {
+        public string HelperText { get; set; }
+
+        public HelperTextAttribute(string helperText)
+        {
+            HelperText = helperText;
+        }
+        
+        public void OnMetadataCreated(ModelMetadata metadata)
+        {
+            metadata.AdditionalValues["helpertext"] = HelperText;
+        }
+    } 
+}
---
+++ BlankMVCProject/Twitter.Bootstrap.Utils/Extensions/BootstrapHtmlExtensions.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Web.Mvc;
+
+namespace Twitter.Bootstrap.Utils.Extensions
+{
+    public static class BootstrapHtmlExtensions
+    {        
+        public static MvcHtmlString BootstrapFormHelpTextBlock(this HtmlHelper htmlHelper, string helpText)
+        {
+            return BootstrapFormHelpText(helpText, "help-block");            
+        }
+
+        public static MvcHtmlString BootstrapFormHelpTextBlockFor<TModel, TValue>(this HtmlHelper<TModel> html, Expression<Func<TModel, TValue>> expression)
+        {
+            ModelMetadata metadata = ModelMetadata.FromLambdaExpression(expression, html.ViewData);
+
+            string helpText = GetHelperText(metadata);
+                
+            return !string.IsNullOrEmpty(helpText) ? html.BootstrapFormHelpTextBlock(helpText) : MvcHtmlString.Empty;                
+        }
+
+        public static MvcHtmlString BootstrapFormHelpTextInline(this HtmlHelper htmlHelper, string helpText)
+        {
+            return BootstrapFormHelpText(helpText, "help-inline");
+        }
+
+        public static MvcHtmlString BootstrapFormHelpTextInlineFor<TModel, TValue>(this HtmlHelper<TModel> html, Expression<Func<TModel, TValue>> expression)
+        {
+            ModelMetadata metadata = ModelMetadata.FromLambdaExpression(expression, html.ViewData);
+
+            string helpText = GetHelperText(metadata);
+
+            return !string.IsNullOrEmpty(helpText) ? html.BootstrapFormHelpTextInline(helpText) : MvcHtmlString.Empty;
+        }
+
+        private static string GetHelperText(ModelMetadata metadata)
+        {
+            string helpText = null;
+
+            if (metadata.AdditionalValues.ContainsKey("helpertext"))
+                helpText = metadata.AdditionalValues["helpertext"] as string;
+
+            return helpText;
+        }
+
+        private static MvcHtmlString BootstrapFormHelpText(string helpText, string cssClass)
+        {
+            var p = new TagBuilder("p");
+            p.AddCssClass(cssClass);
+            p.InnerHtml = helpText;
+
+            return MvcHtmlString.Create(p.ToString());
+        }
+    }
+
+}
