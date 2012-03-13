dc3d8ab7e80fb601825d672f59150d9953bffdee
Gary K <garyk46@gmail.com>
2012-03-13 14:25:36 +0000

    Changed code templates. Added jquery date picker control. Switched to the bootstrap class names for validation.

---
+++ BlankMVCProject/BlankMVCProject/Scripts/jquery.validate.bootstrap.js
@@ -0,0 +1,8 @@
+﻿$.validator.setDefaults({
+    highlight: function (element) {
+        $(element).closest(".control-group").addClass("error");
+    },
+    unhighlight: function (element) {
+        $(element).closest(".control-group").removeClass("error");
+    }
+});
