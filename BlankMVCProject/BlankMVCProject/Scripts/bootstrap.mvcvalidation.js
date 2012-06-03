469329d99adba6a35129fdbca06ecb153a30b320
Ignacio Fuentes <nacho10f@gmail.com>
2012-06-03 16:51:14 -0430

    labels now show up in red. Now we need to remove the unnecessary css classes that were added to mimic bootsrap style.

---
+++ BlankMVCProject/BlankMVCProject/Scripts/bootstrap.mvcvalidation.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function() {
+    $('form').addTriggersToJqueryValidate().triggerElementValidationsOnFormValidation();
+    $('input').elementValidation(function (element, result) {
+        var el = $(element);
+        if (el.hasClass('input-validation-error')) {
+            el.parents('div.control-group').addClass('error');
+        } else {
+            el.parents('div.control-group').removeClass('error');
+        }
+    });
+}); 
